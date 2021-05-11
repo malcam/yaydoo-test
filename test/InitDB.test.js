81c0118b7d3b74b5756aa5e6b2f94d40d0df60cf
Jesus Fernando Almanza <jf_almanza0025@hotmail.com>
2021-05-11 11:51:31 -0500

    Fix test timeout and parameters

--- test/InitDB.test.js
+++ test/InitDB.test.js
@@ -11,10 +11,10 @@ chai.use(chaiHttp);
 
 // eslint-disable-next-line no-undef
 describe('Productos', () => {
-  // describe();
   // eslint-disable-next-line no-undef
-  describe('Bajo promesa', () => {
-    it('Debe crear la base de datos', async () => {
+  describe('Inicialización de base de datos', () => {
+    // eslint-disable-next-line no-undef
+    it('Debe crear la base de datos con contenido', async () => {
       const res = await chai.request(server)
         .post('/api/init');
       res.should.have.status(201);
@@ -26,17 +26,29 @@ describe('Productos', () => {
     it('Debe devolver todos los productos agregados', (done) => {
       chai.request(server)
         .get('/api/products')
-        .end((req, res) => {
+        .end((err, res) => {
+          if (err) done(err);
           res.should.have.status(200);
           done();
         });
-    }).timeout(5000);
+    }).timeout(10000);
     // eslint-disable-next-line no-undef
     it('Debe fallar si los arreglos están vacios', (done) => {
       chai.request(server)
         .get('/api/products')
-        .end((req, res) => {
+        .end((err, res) => {
+          if (err) done(err);
           res.should.have.status(500);
+          done();
+        });
+    });
+    // eslint-disable-next-line no-undef
+    it('Debe responder con un Objeto JSON', (done) => {
+      chai.request(server)
+        .get('/api/products')
+        .end((err, res) => {
+          if (err) done(err);
+          res.body.should.be.a('object');
           done();
         });
     });
